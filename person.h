608f980b3302262f675218d082f40a56639e8ad7
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2024-01-09 23:33:12 +0000

    updated get and set functions

--- person.h
+++ person.h
@@ -21,12 +21,29 @@ class Person {
         string email;
 
     public:
-        string getName();
-        void setName(string name);
-        string getAddress();
-        void setAddress(string address);
-        string getEmail();
-        void setEmail(string email);
+        string getName(){
+            return name;
+        };
+
+        void setName(string n){
+            name = n;
+        };
+
+        string getAddress(){
+            return address;
+        };
+
+        void setAddress(string a){
+            address = a;
+        };
+
+        string getEmail(){
+            return email;
+        };
+
+        void setEmail(string e){
+            email = e;
+        };
 };
 
 class Librarian : public Person {
@@ -36,16 +53,42 @@ class Librarian : public Person {
 
     public:
         Librarian(int staffID, string name, string address,
-        string email, int salary);
-        void addMember();
-        void issueBook(int memberID, int bookID);
-        void returnBook(int memberID, int bookID);
-        void displayBorrowedBooks(int memberID);
+        string email, int salary){
+            this->staffID = staffID;
+            this->salary = salary;
+        };
+        void addMember(){
+
+        };
+
+        void issueBook(int memberID, int bookID){
+
+        };
+
+        void returnBook(int memberID, int bookID){
+
+        };
+
+        void displayBorrowedBooks(int memberID){
+            
+        };
+
         void calcFine(int memberID);
-        int getStaffID();
-        void setStaffID(int staffID);
-        int getSalary();
-        void setSalary(int salary);
+        int getStaffID(){
+            return staffID;
+        };
+
+        void setStaffID(int sID){
+            staffID = sID;
+        };
+
+        int getSalary(){
+            return salary;
+        };
+
+        void setSalary(int sal){
+            salary = sal;
+        };
 };
 
 
@@ -56,9 +99,17 @@ class Member : public Person {
 
     public:
         Member(int memberID, string name, string address,
-        string email);
-        int getMemberID();
-        vector<Book> getBooksBorrowed();
+        string email){
+            this->memberID = memberID;
+        };
+        int getMemberID(){
+            return memberID;
+        };
+
+        vector<Book> getBooksBorrowed(){
+            return booksLoaned;
+        };
+
         void setBooksBorrowed(Book book);
 
 };
@@ -76,10 +127,22 @@ class Book : public Librarian, public Member {
     public:
         Book(int bookID, string bookName, string authorFirstName,
         string authorLastName);
-        int getBookID();
-        string getBookName();
-        string getAuthorFirstName();
-        string getAuthorLastName();
+        int getBookID(){
+            return bookID;
+        };
+
+        string getBookName(){
+            return bookName;
+        };
+
+        string getAuthorFirstName(){
+            return authorFirstName;
+        };
+    
+        string getAuthorLastName(){
+            return authorLastName;
+        };
+
         void setDueDate();
         void returnBook();
         void borrowBook(Member borrower);
